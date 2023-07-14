f47abf5204ceb9bb6d5e751391023cce43d0a951
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-07-14 18:51:01 +0200

    first commit

---
+++ app/src/main/java/com/example/pizzatask/PizzaDetailsScreen.kt
@@ -0,0 +1,17 @@
+package com.example.pizzatask
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pizzatask.ui.theme.PizzaTaskTheme
+
+@Composable
+fun PizzaDetailsScreen() {
+}
+
+@Preview
+@Composable
+fun PizzaDetailsScreenPreview(){
+    PizzaTaskTheme {
+        PizzaDetailsScreen()
+    }
+}
